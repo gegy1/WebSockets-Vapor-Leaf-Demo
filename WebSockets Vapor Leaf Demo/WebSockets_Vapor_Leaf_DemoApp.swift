5cf596caac5dd7b363e10fb3ad61c50785bcb3fe
Georg D <gegyman@gmail.com>
2025-04-21 15:13:47 +0200

    Initial Commit

---
+++ WebSockets Vapor Leaf Demo/WebSockets_Vapor_Leaf_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSockets_Vapor_Leaf_DemoApp.swift
+//  WebSockets Vapor Leaf Demo
+//
+//  Created by Georg Draxler on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct WebSockets_Vapor_Leaf_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
